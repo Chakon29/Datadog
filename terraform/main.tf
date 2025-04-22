389aec63f0e0318e677d42325d95ad0c0a389c86
Vicente Chacon <vicente.chacon@sovos.com>
2025-04-22 16:57:33 -0400

    fix format

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "datadog" {
-  api_key = var.datadog_api_key  
-  app_key = var.datadog_app_key  
-  api_url = var.datadog_api_url  
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+  api_url = var.datadog_api_url
 }
