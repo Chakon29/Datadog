043fb7f5e44fdded9fe89c440c0886937ce0900c
Vicente Chacon <vicente.chacon@sovos.com>
2025-04-23 17:02:44 -0400

    delete no data monitors old

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,3 @@
-output "service_availability_monitor_id" {
-  description = "ID del monitor de disponibilidad del servicio"
-  value       = datadog_monitor.service_availability.id
-}
-
-output "service_availability_monitor_url" {
-  description = "URL del monitor de disponibilidad del servicio"
-  value       = "https://app.datadoghq.com/monitors/${datadog_monitor.service_availability.id}"
-}
-
-output "response_time_monitor_id" {
-  description = "ID del monitor de tiempo de respuesta"
-  value       = datadog_monitor.response_time.id
-}
-
-output "response_time_monitor_url" {
-  description = "URL del monitor de tiempo de respuesta"
-  value       = "https://app.datadoghq.com/monitors/${datadog_monitor.response_time.id}"
-}
-
 # Monitores de pruebas de disponibilidad
 output "sitio_siempre_disponible_test_id" {
   description = "ID del test sintético para el sitio siempre disponible (200)"
