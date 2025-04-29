f368fdd0433fedd94e5d235af64ba0660463a367
Vicente Chacon <vicente.chacon@sovos.com>
2025-04-29 16:16:50 -0400

    real fix monitor de anomalía de CPU

--- terraform/service_monitors.tf
+++ terraform/service_monitors.tf
@@ -3,7 +3,7 @@ resource "datadog_monitor" "VM_GCP_Uso_de_cpu_Anomal" {
   require_full_window = false
   monitor_thresholds {
     critical = 0.6
-    critical_recovery = 0
+    critical_recovery = 0.3  # Cambiado de 0 a 0.3 para evitar superposición
   }
   monitor_threshold_windows {
     recovery_window = "last_15m"
@@ -17,11 +17,20 @@ EOT
   message = <<EOT
 @team-teleton 
 ALERTA: Anomalia en uso de CPU en {{host.name}} - {{value}}% 
-
 Detalles:
 - Host: {{host.name}}
 - CPU usado: {{value}} %
 - Proyecto: ancient-tractor-452505-b5
 - Timestamp: {{last_triggered_at}}
 EOT
+  
+  # Etiquetas para mantener consistencia
+  tags = [
+    "type:vm",
+    "env:${var.environment}",
+    "managed-by:terraform",
+    "provider:gcp"
+  ]
+  
+  include_tags = true
 }
