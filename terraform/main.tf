721f4bbacebbff341a99f9da10c325b8c841365c
Vicente Chacon <vicente.chacon@sovos.com>
2025-05-02 12:19:11 -0400

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,21 +12,3 @@ provider "datadog" {
   app_key = var.datadog_app_key
   api_url = var.datadog_api_url
 }
-
-# Módulos para importación de monitores
-module "imported_monitors" {
-  source = "./monitors/imported_monitors"
-  count  = length(var.monitor_ids)
-  
-  monitor_id     = var.monitor_ids[count.index]
-  monitor_params = var.monitor_params[count.index]
-}
-
-# Módulos para importación de dashboards
-module "imported_dashboards" {
-  source = "./dashboards/imported_dashboards"
-  count  = length(var.dashboard_ids)
-  
-  dashboard_id = var.dashboard_ids[count.index]
-  dashboard    = file("./dashboards/imported_dashboards/${var.dashboard_names[count.index]}.json")
-}
