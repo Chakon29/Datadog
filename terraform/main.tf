7d11ba54a5766dd2b57582ac49e1b303703c5087
Vicente Chacon <vicente.chacon@sovos.com>
2025-05-05 17:26:24 -0400

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,11 @@ import {
   to = module.monitor_1.datadog_monitor.VM_GCP_Uso_de_disco_Critical
   id = "3616409"
 }
+
 module "monitor_1" {
-	source = "./monitors/1_monitor"
-	providers = {
-		datadog = datadog
-	}
-	monitor_params = var.monitor_parametros[0]  # Note the [0] index here
+  source = "./monitors/1_monitor"
+  providers = {
+    datadog = datadog
+  }
+  monitor_params = var.monitor_parametros[0]
 }
--- terraform/monitors/1_monitor/output.tf
+++
@@ -1,5 +0,0 @@
-# Import existing monitor into terraform state
-import {
-  to = datadog_monitor.VM_GCP_Uso_de_disco_Critical
-  id = "3616409"  # Same ID as in monitors.tfvars
-}
