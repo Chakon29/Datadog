fc7a504aa38317caa449b5d62cce6e0b1091a700
Vicente Chacon <vicente.chacon@sovos.com>
2025-05-05 16:15:09 -0400

    fix import?

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
-import {
-  to = module.monitor_1.datadog_monitor.VM_GCP_Uso_de_disco_Critical
-  id = "1"
-}
+#import {
+#  to = module.monitor_1.datadog_monitor.VM_GCP_Uso_de_disco_Critical
+#  id = "1"
+#}
 module "monitor_1" {
 	source = "./monitors/1_monitor"
 	providers = {
